7de45068bbc5b80c316ee323a819317563ee05f2
sanifaaaa <115127468+sanifaaaa@users.noreply.github.com>
2022-10-27 11:08:50 +0700

    MENAMBAHKAN LATIHAN
    menambahkan latihan algoritma dan pemrograman

---
+++ c ++/coba.1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+main()
+{
+    int a = 13;
+    int b = 2;
+    char c [15] = "sanifa lihawa";
+
+    puts("nama");
+    puts(c);
+    printf("terdiri dari %d hurup dan %d kata", a, b);
+}
---
+++ php/tipedata.php
@@ -0,0 +1,19 @@
+<?php
+$nim='A';
+$nama="sanifa lihawa";
+$umur=18;
+$nilai=82.25;
+$status=TRUE;
+
+echo"---Belajar Tipe Data---<br><br>";
+echo"Tipe Data Char:".$nim."<br>;
+echo"Tipe Data String:$nama<br>";
+print"Tipe Data Int:".$umur;
+print"<br>";
+printf("Tipe Data Float :%.5f<br><br>",$nilai);
+echo"Tipe Data Boolean :<br><br>";
+if($Status)
+    echo""Status:Aktif";
+else 
+    echo"Status:Tidak Aktif";
+?>
